7ce895bd0615368b02433f5e843db8e559e78441
Dev <dev.12092003@gmail.com>
2024-08-27 00:43:42 +0530

    ttttttttttttttttttt

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 
-
 function App() {
   const [mode,setMode] = useState('light') // whether the dark mode is enable or not 
-
+  const [alert, setAlert] = useState(null)
+  
   const toggleMode = ()=>{
-    if(mode=='light'){
+    if(mode==='light'){
       setMode('dark');
-      document.body.style.backgroundColor='gray';
+      document.body.style.backgroundColor='#212224';
+      showAlert("Dark mode is enable","success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor='white';
-      
+      showAlert("Light mode is enable","success"); 
     }
+  }
+
+  const showAlert = (message,type)=>{ ///this function use for the show the alert message
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },2000);
   }
 
   return (
     <>
       <Navbar title="TextUtils" aboutText="About us" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
       <div className="container my-3">
-        <TextForm heading="Enter Text to analyze" mode={mode}/>
+        <TextForm showAlert={showAlert} heading="Enter Text to analyze" mode={mode}/>
         {/* <About/> */}
       </div>
       
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import {CopyToClipboard} from 'react-copy-to-clipboard'
+//import {CopyToClipboard} from 'react-copy-to-clipboard'
 
 
 const TextForm = (props) => {
@@ -7,6 +7,7 @@ const TextForm = (props) => {
         //console.log("Upper click clicked" + text);
         let newText=text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to UpperCase","success");  // this is come from app.js inistilize this
     }
     const handleOnChange= (event)=>{ // thi is evenrt handler 
         //console.log("On change");
@@ -15,20 +16,24 @@ const TextForm = (props) => {
     const handleLowClick= ()=>{
         let newtText=text.toLowerCase();
         setText(newtText);
+        props.showAlert("Converted to LowerCase","success");
     }
     const handleClearClick = ()=>{
         let newText=" ";
         setText(newText);
+        props.showAlert("Text Cleared","success");
     }
     const handleCopyClick = ()=>{
         var newtext=document.getElementById("myBox");
         newtext.select();
         navigator.clipboard.writeText(newtext.value);
+        props.showAlert("Text Copied","success");
     }
 
     const handleExtraSpace = ()=>{
         let newtext=text.split(/[ ]+/);
         setText(newtext.join(" "));
+        props.showAlert("Removed Extra Space","success");
     }
 
     const [text, setText] = useState('');
@@ -40,7 +45,7 @@ const TextForm = (props) => {
         <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control " value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor:props.mode==='dark'?'gray':'white' , color:props.mode==='dark'?'white':'black'}}></textarea>
+                <textarea className="form-control " value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor:props.mode==='dark'?'#212224':'white' , color:props.mode==='dark'?'white':'black'}}></textarea>
             </div>
             <button className='btn btn-primary mx-1' onClick={handleUpClick}>Conver to Uppercase</button>
             <button className='btn btn-primary mx-1' onClick={handleLowClick}>Conver to Lowercase</button>
